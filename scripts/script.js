65263f51575d97643f8d36b55ad31713b0e91f1d
TimDisseldorp1998 <79916293+TimDisseldorp1998@users.noreply.github.com>
2021-12-24 18:44:16 +0100

    Add files via upload
    Responsive fase 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-// JavaScript Document
+(function (icon) {
+    var menu = document.querySelector('.js-menu');
+
+    icon.addEventListener('click', openMenu);
+
+    function openMenu() {
+        menu.classList.toggle('nav-pages--open');
+    }
+
+})(document.querySelector('.js-hamburger'));
