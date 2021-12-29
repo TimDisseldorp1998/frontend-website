70ce69b44280cf1efcdd6d8d5a19724ac5c44b47
TimDisseldorp1998 <79916293+TimDisseldorp1998@users.noreply.github.com>
2021-12-29 22:20:34 +0100

    Heren pagina - update

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,14 @@
     }
 
 })(document.querySelector('.js-hamburger'));
+
+(function (icon) {
+    var menu = document.querySelector('.js-element');
+
+    icon.addEventListener('click', openMenu);
+
+    function openMenu() {
+        menu.classList.toggle('filter-dropdown__container--open');
+    }
+
+})(document.querySelector('.js-button'));
